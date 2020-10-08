a309b59b5736b4787d4ee614684e7a4fded8eade
Creator <rishirajsinghchhabra@gmail.com>
2020-10-09 00:36:29 +0530

    Bot initlize and Dm send with user data

---
+++ moropy_bot/bot.py
@@ -0,0 +1,30 @@
+# bot.py
+import os
+import random
+from discord.ext import commands
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+bot = commands.Bot(command_prefix='!')
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+
+@bot.command(name="register", help="Start user registration")
+async def on_message(ctx):
+    user = ctx.message.author
+    channel = await user.create_dm()
+    print(user.roles)
+    response = f"Starting user registration for user {user.id} with roles {user.roles}"
+    await channel.send(response)
+
+
+bot.run(TOKEN)
