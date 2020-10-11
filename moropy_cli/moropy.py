801b319ad2f6b3534bb157e64c9b43530f7776ca
Aayush Joglekar <aayushjog@gmail.com>
2020-10-11 10:21:07 +0530

    [cli] Initialise register command
    [cli] ran formatter

--- moropy_cli/moropy.py
+++ moropy_cli/moropy.py
@@ -1,11 +1,27 @@
 #! ./venv/bin/python3
 
+import os
+import pathlib
+
 import click
+
+root_dir = ""
 
 
 @click.command('register')
-def register_user():
-    click.echo("I am registering")
+@click.option('-c', '--code', required=True)
+def register_user(code):
+
+    credentials_file_name = "creds"
+
+    credentials_file_path = os.path.join(root_dir, credentials_file_name)
+
+    if not os.path.exists(credentials_file_path):
+        with open(credentials_file_path, "w") as f:
+            f.write(code)
+        click.echo("Successfully registered! You can continue with checking in!")
+    else:
+        click.echo("Already registered!")
 
 
 @click.command('checkin')
@@ -30,7 +46,16 @@ def set_available():
 
 @click.group()
 def init_cli():
-    pass
+
+    print("Initalizing Moropy")
+    user_home_dir = pathlib.Path.home()
+
+    global root_dir
+    root_dir = pathlib.Path.joinpath(user_home_dir, ".moropy")
+
+    if not os.path.exists(root_dir):
+        os.mkdir(root_dir)
+        click.echo("generated {}".format(root_dir))
 
 
 init_cli.add_command(register_user)
@@ -38,7 +63,6 @@ init_cli.add_command(checkin)
 init_cli.add_command(checkout)
 init_cli.add_command(set_away)
 init_cli.add_command(set_available)
-
 
 if __name__ == '__main__':
     init_cli()
