0940bd4a7f86f051408b4d352da1d46cc62bd096
Aayush Joglekar <aayushjog@gmail.com>
2020-10-11 17:35:55 +0530

    [cli] Finalise log collection

--- moropy_cli/ticker.py
+++ moropy_cli/ticker.py
@@ -18,6 +18,19 @@ status_file_name = "status"
 status_file_path = os.path.join(moropy_dir, status_file_name)
 
 
+def write_to_file(filename, process, start_time):
+
+    if len(process) == 0:
+        return
+
+    end_time = datetime.datetime.utcnow()
+    duration = (end_time - start_time).total_seconds()
+
+    with open(filename, "a") as window_logs:
+        window_logs.write("{},{},{},{}\n".format(previous_window, start_time, end_time, duration))
+
+
+
 # writes active status to moropy.sh
 with open(status_file_path, 'w') as file:
     file.write("1")
@@ -27,6 +40,9 @@ ticker_continue = True
 previous_window = ""
 previos_start_time = datetime.datetime.utcnow()
 
+with open("logs.csv", "w") as window_logs:
+    pass
+
 while ticker_continue:
 
     # reads the status from moropy.sh
@@ -35,7 +51,9 @@ while ticker_continue:
         ticker_continue = bool(int(script[0].decode('UTF-8')))
 
     if not ticker_continue:
+        write_to_file("logs.csv", previous_window, previos_start_time)
         break
+
 
     # if ticker_continue is false, ticker will stop after this iteration
 
@@ -49,13 +67,11 @@ while ticker_continue:
 
     process_name = subprocess.getoutput(process_name_cmd)
 
-    if process_name != previous_window and len(process_name) > 0:
+    if process_name != previous_window:
 
         with open("logs.csv", "a") as window_logs:
 
-            time_delta = (datetime.datetime.utcnow() - previos_start_time).total_seconds()
-
-            window_logs.write("{},{}\n".format(previous_window, time_delta))
+            write_to_file("logs.csv", previous_window, previos_start_time)
 
             previous_window = process_name
             previos_start_time = datetime.datetime.utcnow()
