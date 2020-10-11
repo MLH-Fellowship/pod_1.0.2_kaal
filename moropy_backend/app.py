af0d05ab4a4b9080d675910cd4d911f654cef1de
Creator <rishirajsinghchhabra@gmail.com>
2020-10-11 11:06:22 +0530

    Get User route add

--- moropy_backend/app.py
+++ moropy_backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
-from firebase_upload import upload
+from firebase_upload import upload, get_user
 app = Flask(__name__)
 
 
@@ -10,13 +10,20 @@ def welcome():
     return jsonify({"status": "The API is healthy and working properly"})
 
 
-@app.route('/user/', methods=['POST'])
+@app.route('/register/', methods=['POST'])
 def register_user():
     input_json = request.get_json(force=True)
     print("USER ID", input_json['user_id'])
     print("ROLES", input_json['roles'])
     userHash = upload(input_json['user_id'], input_json['roles'])
     return jsonify({"statusCode": 200, "userHash": userHash})
+
+
+@app.route('/<string:userHash>', methods=['GET'])
+def get_user_details(userHash):
+    print(userHash)
+    resp = get_user(userHash)
+    return jsonify({'user': resp})
 
 
 if __name__ == '__main__':
--- moropy_backend/firebase_services.py
+++ moropy_backend/firebase_services.py
@@ -7,14 +7,24 @@ cred = credentials.Certificate('firebase-key.json')
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+user_ref = db.collection(u'users')
 
 
 def upload(userID, roles):
     print('working')
     userHash = uuid.uuid4()
-    doc_ref = db.collection(u'users').document(f'{userHash}')
+    doc_ref = user_ref.document(f'{userHash}')
     doc_ref.set({
         u'roles': roles,
         u'discordId': userID
     })
     return userHash
+
+
+def get_user(userHash):
+    print("getting user")
+    users = user_ref.stream()
+    for user in users:
+        if(user.id == userHash):
+            print(user.to_dict())
+            return user.to_dict()
