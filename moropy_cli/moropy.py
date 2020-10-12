cec5eab76b6b5428239d64b8b9501c6b745f90df
Aayush Joglekar <aayushjog@gmail.com>
2020-10-12 20:31:47 +0530

    [cli] Initialise CLI commands (#18)
    * [cli] Initialise register command
    
    [cli] ran formatter
    
    * [cli] Complete background script
    
    * [cli] Initialise background script
    
    * [cli] Complete checkin command
    
    * [cli] Complete checkout command
    
    * [cli] Update status file path
    
    * [cli] Configure requests in register
    
    * [cli] Redirect windows to file
    
    * [cli] Integrated checkin & checkout
    
    * [cli] Save logs with duration in logs.csv
    
    * [cli] Finalise log collection
    
    * [cli] reformat the code
    
    * [cli] Push activity data to database
    
    * [backend] Update API routes (#22)
    
    * [cli] Configured backend
    
    * [cli] Push activity data to firestore
    
    * [cli] Initialise register command
    
    [cli] ran formatter
    
    * [cli] Complete background script
    
    * [cli] Initialise background script
    
    * [cli] Complete checkin command
    
    * [cli] Complete checkout command
    
    * [cli] Update status file path
    
    * [cli] Configure requests in register
    
    * [cli] Redirect windows to file
    
    * [cli] Integrated checkin & checkout
    
    * [cli] Save logs with duration in logs.csv
    
    * [cli] Finalise log collection
    
    * [cli] reformat the code
    
    * [cli] Push activity data to database
    
    * [cli] Configured backend
    
    * [cli] Push activity data to firestore
    
    * [cli] Push activity data to firestore
    
    * [cli] Fix issues
    
    Co-authored-by: Rishi Raj Singh <49035175+rish07@users.noreply.github.com>

--- moropy_cli/moropy.py
+++ moropy_cli/moropy.py
@@ -1,36 +1,132 @@
 #! ./venv/bin/python3
 
+import json
+import os
+import pathlib
+import subprocess
+
 import click
+import requests
+
+root_dir = ""
+credentials_file_name = "creds"
+status_file_name = "status"
+base_url = "https://kaal-backend.herokuapp.com"
 
 
 @click.command('register')
-def register_user():
-    click.echo("I am registering")
+@click.option('-c', '--code', required=True)
+def register_user(code):
+    credentials_file_path = os.path.join(root_dir, credentials_file_name)
+
+    # section to contact the backend API to validate code
+
+    response = requests.post(
+        "{}/validate".format(base_url), json.dumps({"userHash": code})
+    )
+
+    if response.status_code == 401:
+        print("Invalid Token. Please try again!")
+        return
+
+    if response.status_code != 200:
+        print("Server error. please contact admin.")
+        return
+
+    response_data = response.json()
+
+    print(response_data)
+
+    username = "zerefwayne"
+
+    with open(credentials_file_path, "w") as file:
+        file.write(code)
+        file.write('\n')
+        file.write(username)
+
+    click.echo(
+        "Successfully registered {}! You can continue with checking in!".format(
+            username
+        )
+    )
 
 
 @click.command('checkin')
 def checkin():
-    click.echo("I am checking in!")
+    credentials_file_path = os.path.join(root_dir, credentials_file_name)
+
+    with open(credentials_file_path, "rb") as file:
+        user_hash = file.readline().decode('UTF-8')
+        user_name = file.readline().decode('UTF-8')
+
+        print("Checking in for", user_name, user_hash)
+
+    subprocess.call("chmod +x ./ticker.py", shell=True)
+    # subprocess.call("nohup ./ticker.py >/dev/null 2>&1 &", shell=True)
+    subprocess.call("nohup ./ticker.py &", shell=True)
 
 
 @click.command('checkout')
 def checkout():
-    click.echo("I am checking out!")
+    status_file_path = os.path.join(root_dir, status_file_name)
+
+    if not os.path.exists(status_file_path):
+        print(status_file_path, "doesn't exist")
+        return
+
+    # writes close status to moropy.sh
+    with open(status_file_path, 'w') as file:
+        file.write("0")
 
 
 @click.command('away')
 def set_away():
-    click.echo("I am away!")
+
+    click.echo("Setting away")
+
+    credentials_file_path = os.path.join(root_dir, credentials_file_name)
+
+    with open(credentials_file_path, "rb") as file:
+        user_hash = file.readline().decode('UTF-8')[:-1]
+        # user_name = file.readline().decode('UTF-8')
+
+        data = {'userHash': user_hash, 'status': 'away'}
+
+        res = requests.post("{}/status/".format(base_url), json.dumps(data))
+
+        print(res.json())
 
 
 @click.command('available')
 def set_available():
-    click.echo("I am available!")
+
+    click.echo("Setting available")
+
+    credentials_file_path = os.path.join(root_dir, credentials_file_name)
+
+    with open(credentials_file_path, "rb") as file:
+        user_hash = file.readline().decode('UTF-8')[:-1]
+        # user_name = file.readline().decode('UTF-8')
+
+        data = {'userHash': user_hash, 'status': 'available'}
+
+        res = requests.post("{}/status/".format(base_url), json.dumps(data))
+
+        print(res.json())
 
 
 @click.group()
 def init_cli():
-    pass
+
+    print("Initalizing Moropy")
+    user_home_dir = pathlib.Path.home()
+
+    global root_dir
+    root_dir = pathlib.Path.joinpath(user_home_dir, ".moropy")
+
+    if not os.path.exists(root_dir):
+        os.mkdir(root_dir)
+        click.echo("generated {}".format(root_dir))
 
 
 init_cli.add_command(register_user)
@@ -38,7 +134,6 @@ init_cli.add_command(checkin)
 init_cli.add_command(checkout)
 init_cli.add_command(set_away)
 init_cli.add_command(set_available)
-
 
 if __name__ == '__main__':
     init_cli()
---
+++ moropy_cli/ticker.py
@@ -0,0 +1,128 @@
+#! /usr/bin/python3
+
+import datetime
+import json
+import os
+import pathlib
+import subprocess
+import time
+
+import requests
+
+# The ticker script creates a moropy.sh file in the tmp folder
+# to keep the status of ticker. 0 - ticker should stop,
+# 1 - ticker should keep running
+# At each second the ticker will check the status from the file
+# and then execute the command of getting the active window.
+
+root_dir = pathlib.Path.home()
+moropy_dir = os.path.join(root_dir, ".moropy")
+status_file_name = "status"
+status_file_path = os.path.join(moropy_dir, status_file_name)
+flush_file_path = "flush.csv"
+userhash = "heeelo"
+base_url = "https://kaal-backend.herokuapp.com"
+
+
+def write_to_file(filename, process, start_time):
+
+    if len(process) == 0:
+        return
+
+    end_time = datetime.datetime.utcnow()
+    duration = (end_time - start_time).total_seconds()
+
+    with open(filename, "a") as window_logs:
+        window_logs.write(
+            "{},{},{},{}\n".format(previous_window, start_time, end_time, duration)
+        )
+
+
+def push_to_database():
+    activities = []
+    with open("logs.csv", "rb") as file:
+
+        while True:
+
+            line = file.readline()
+
+            if not line:
+                break
+
+            process, startTime, endTime, duration = line.decode('UTF-8').split(',')
+
+            activities.append(
+                {
+                    "name": process,
+                    "start_time": startTime,
+                    "end_time": endTime,
+                    "duration": int(float(duration)),
+                }
+            )
+
+    payload = {"userHash": userhash, "activities": activities}
+
+    payload_json = json.dumps(payload)
+
+    response = requests.post("{}/storeactivity/".format(base_url), payload_json)
+
+    print(response.status_code)
+
+    with open(flush_file_path, "w") as f:
+        f.write(payload_json)
+
+    with open("logs.csv", "w") as file:
+        pass
+
+
+# writes active status to moropy.sh
+with open(status_file_path, 'w') as file:
+    file.write("1")
+
+ticker_continue = True
+
+previous_window = ""
+previos_start_time = datetime.datetime.utcnow()
+previos_push_time = datetime.datetime.utcnow()
+
+with open("logs.csv", "w") as window_logs:
+    pass
+
+while ticker_continue:
+
+    # reads the status from moropy.sh
+    with open(status_file_path, 'rb') as file:
+        script = file.readlines()
+        ticker_continue = bool(int(script[0].decode('UTF-8')))
+
+    if not ticker_continue:
+        write_to_file("logs.csv", previous_window, previos_start_time)
+        push_to_database()
+        break
+
+    if (datetime.datetime.utcnow() - previos_push_time).total_seconds() >= 1 * 60:
+        push_to_database()
+        previos_push_time = datetime.datetime.utcnow()
+
+    # if ticker_continue is false, ticker will stop after this iteration
+
+    cmd = "xdotool getwindowfocus getwindowname getwindowpid"
+
+    output = subprocess.getoutput(cmd)
+
+    pid = output.split('\n')[1]
+
+    process_name_cmd = "ps -p {} -o comm=".format(pid)
+
+    process_name = subprocess.getoutput(process_name_cmd)
+
+    if process_name != previous_window:
+
+        with open("logs.csv", "a") as window_logs:
+
+            write_to_file("logs.csv", previous_window, previos_start_time)
+
+            previous_window = process_name
+            previos_start_time = datetime.datetime.utcnow()
+
+    time.sleep(0.5)
