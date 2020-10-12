8b891e6f0349beb5fe7fcfa2fae68654f9dc67c0
Rishi Raj Singh <49035175+rish07@users.noreply.github.com>
2020-10-12 15:02:34 +0530

    Remove print statements

--- moropy_backend/firebase_services.py
+++ moropy_backend/firebase_services.py
@@ -13,8 +13,6 @@ batch = db.batch()
 
 
 def upload(userID, roles, userName):
-    print('working')
-
     userHash = uuid.uuid4()
     doc_ref = user_ref.document(f"{userHash}")
     doc_ref.set({u'roles': roles, u'discordId': userID, u'userName': userName, u'status': 'Away'})
@@ -22,29 +20,21 @@ def upload(userID, roles, userName):
 
 
 def get_user(userHash):
-    print("getting user")
-    print(userHash)
     users = user_ref.stream()
     for user in users:
         if user.id == userHash:
-
-            print(user.to_dict())
             return user.to_dict()
     return 'User not found'
 
 
 def store_activity(userHash, activity):
-    print('storing activity')
     doc_ref = user_ref.document(f'{userHash}').collection('activity')
     for i in range(len(activity)):
         doc_ref.document().set(activity[i])
-
-    print('done')
     return "working"
 
 
 def update(userHash, status):
-    print('updating status')
     doc_ref = user_ref.document(f'{userHash}')
     doc_ref.set(
         {
