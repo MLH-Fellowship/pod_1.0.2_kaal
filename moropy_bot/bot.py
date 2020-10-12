4ef89b5f9bc062939de6539dbcf0c6e795dedf91
Creator <rishirajsinghchhabra@gmail.com>
2020-10-12 10:08:02 +0530

    Deploy backend

--- moropy_bot/bot.py
+++ moropy_bot/bot.py
@@ -1,14 +1,15 @@
 # bot.py
 import os
-import requests
+
 import discord
+import requests
 from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-baseURL = "https://9157b335f219.ngrok.io"
+baseURL = "https://kaal-backend.herokuapp.com/"
 
 client = discord.Client()
 bot = commands.Bot(command_prefix='!')
@@ -26,17 +27,20 @@ async def on_message(ctx):
     roles = []
     for i in user.roles:
         roles.append(i.name)
-    response = f'Starting user registration for user {user} with roles {roles}, please wait...'
+    response = (
+        f'Starting user registration for user {user} with roles {roles}, please wait...'
+    )
     await channel.send(response)
-    r = requests.post(url=baseURL + '/register/', json={
-        "userId": str(user.id),
-        "userName": str(user),
-        "roles": roles
-    })
+    r = requests.post(
+        url=baseURL + '/register/',
+        json={"userId": str(user.id), "userName": str(user), "roles": roles},
+    )
 
     res = r.json()
     print(res)
     userHash = res['userHash']
     resp2 = f'Your key is: {userHash}. Please enter it in the CLI'
     await channel.send(resp2)
+
+
 bot.run(TOKEN)
